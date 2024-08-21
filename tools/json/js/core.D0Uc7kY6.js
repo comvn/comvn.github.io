6aebb362bee1ea62e4a268380954fd11e22b4ba2
comvn <145321168+comvn@users.noreply.github.com>
2024-08-21 14:19:18 +0700

    json tool commit

---
+++ tools/json/js/core.D0Uc7kY6.js
@@ -0,0 +1,75 @@
+<html>
+<head>
+<title>Denied Access Policy </title>
+<meta name="author" content="VPBANK systems">
+<meta name="description" content="Denied Access Policy">
+</head>
+<table style="height: 197px;width: 635px; margin: 0px auto;">
+ <tbody>
+  <tr>
+   <td>
+    <p style="margin-bottom: 0.11in;"><span style="font-family: Times New Roman,serif;"><span style="font-size: medium;"><em><b><a href="javascript:void(0);" style="color: #000000;">Lưu ý:</a></b></em></span></span></p>
+
+    <p style="margin-bottom: 0px;">
+    <span style="font-family: Times New Roman,serif;"><span style="font-size: medium;"><em>1. Dự án sẽ tiến hành khảo sát lần 2 về nhu cầu sử dụng Internet của từng phòng ban theo mẫu sau (đề nghị các phòng chức năng tại Hội sở, các Chi nhánh chủ động nghiên cứu trước).</em></span></span>
+    </p>
+
+    <p style="margin-bottom: 0.11in; float: right;">
+     <a href="http://10.36.22.25/proxy/Prosposed_Whitelist_v2.pdf"><span style="color: #00000a;"><span style="font-family: Times New Roman,serif;"><span style="font-size: medium;"><em>Proposed Whitelist (Danh sách trắng đề xuất)</em></em></span></span></a>
+    </p>
+
+    <br><br>
+
+    <p style="margin-bottom: 0px;">
+     <span style="font-family: Times New Roman,serif;"><span style="font-size: medium;"><em>2. Các đơn vị do yêu cầu đặc thù về nghiệp vụ cần xử lý ngoại lệ đối với các trang web bị chặn cần làm theo hướng dẫn sau:</em></span></span>
+    </p>
+
+    <p style="margin-bottom: 0.11in; float: right;">
+     <a href="http://10.36.22.25/proxy/Quy_trinh_xu_ly_truy_cap_Internet_VIE.pdf"><span style="color: #00000a;"><span style="font-family: Times New Roman,serif;"><span style="font-size: medium;"><em>Quy trình xử lý ngoại lệ truy cập Internet - VIETNAMESE</em></span></span></span></a>
+    </p>
+
+    <br><br>
+
+    <p style="margin-bottom: 0.11in; float: right;">
+     <a href="http://10.36.22.25/proxy/Internet_Acceptance_Usage_Exception_ENG.pdf"><span style="color: #00000a;"><span style="font-family: Times New Roman,serif;"><span style="font-size: medium;"><em>Internet Acceptance Usage Exception - ENGLISH</em></span></span></span></a>
+    </p>
+
+    <br><br>
+
+    <p style="margin-bottom: 0px;">
+     <span style="font-family: Times New Roman,serif;"><span style="font-size: medium;"><em>3. Câu hỏi thường gặp: 
+     <a href="http://10.36.22.25/proxy/FAQ.pdf"><span style="color: #00000a;"><span style="font-family: Times New Roman,serif;"><span style="font-size: medium;"><em>FAQ</em></span></span></span></a>
+     </em></span></span>
+    </p>
+
+   </td>
+  </tr>
+ </tbody>
+</table>
+<center><p style="margin-bottom: 0.11in;"></p>
+<nnstyle="margin-bottom: 0.11in; line-height: 150%;" align="CENTER"><span style="font-family: Times New Roman,serif;"><span style="font-size: medium;"><em><strong>Trân trọng cảm ơn và chúc anh/chị một ngày làm việc hiệu quả!</em></strong></span></span></span></p></center><br><br>
+<table style="height: 197px;width: 635px; margin: 0px auto;">
+ <tbody>
+  <tr>
+   <td>
+    <p style="margin-bottom: 0px;">
+    <span style="font-family: Times New Roman,serif;"><span style="font-size: medium;"><em>Your IP address: 10.39.199.94</em></span></span>
+    </p>
+    <p style="margin-bottom: 0px;">
+    <span style="font-family: Times New Roman,serif;"><span style="font-size: medium;"><em>Your Username: nguyenda2</em></span></span>
+    </p>
+    <p style="margin-bottom: 0.11in; float: center;">
+    <span style="font-family: Times New Roman,serif;"><span style="font-size: medium;"><em>Web Page URL: https:&#x2F;&#x2F;static.addtoany.com&#x2F;menu&#x2F;modules&#x2F;core.D0Uc7kY6.js</em></span></span>
+    </p>
+    <p style="margin-bottom: 0.11in; float: center;">
+    <span style="font-family: Times New Roman,serif;"><span style="font-size: medium;"><em>Web Page Category: Web Ads&#x2F;Analytics</em></span></span>
+    </p>
+    <p style="margin-bottom: 0.11in; float: center;">
+    <span style="font-family: Times New Roman,serif;"><span style="font-size: medium;"><em>Current UTC date/time: 21-08-2024 07:07:09</em></span></span>
+    </p>
+   </td>
+  </tr>
+ </tbody>
+</table>
+</body>
+</html>
